b12cf8b01d4fa58bbade625a216fb99ea7c09e40
sttewiee <plapikovv@gmail.com>
2025-05-26 13:21:53 +0300

    -

---
+++ backend/app/models.py
@@ -0,0 +1,12 @@
+from .db import db
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    comments = db.relationship("Comment", backref="post", lazy=True)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
